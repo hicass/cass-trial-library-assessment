6b54b09b8352473cd6e0f5393bf46ddb268884e1
hicass <hicass.dev@gmail.com>
2025-07-28 19:13:35 -0700

    Add aria labels to the AccommodationRequestFormSection component to improve accessibility, and update the comment and name for the copyData object

--- src/features/accommodationRequest/components/AccommodationRequestFormSection.tsx
+++ src/features/accommodationRequest/components/AccommodationRequestFormSection.tsx
@@ -1,22 +1,29 @@
 import { ContentContainer } from '../../../components/containers/ContentContainer';
 import { AccommodationRequestForm } from './AccommodationRequestForm';
 
-const sectionData = {
+const copyData = {
   title: 'Need Assistance?',
   description:
     "At Mars On My Mind, we believe everyone deserves the chance to explore Mars. If you have a disability or require any special accommodations during your travel experience, please fill out this form. We'll work closely with you to ensure your trip is safe, enjoyable, and tailored to your needs.",
 };
 
-// Component to render the whole Accommodation Request Form section of the page
+// Renders the whole Accommodation Request Form section of the page including
+// a title and description
 export const AccommodationRequestFormSection = () => {
   return (
     <ContentContainer>
-      <section className="flex flex-col items-center justify-between lg:gap-8 lg:flex-row lg:items-start">
+      <section
+        aria-labelledby="accommodation-section-title"
+        className="flex flex-col items-center justify-between lg:gap-8 lg:flex-row lg:items-start"
+      >
         <header className="w-full lg:max-w-lg mt-[4rem]">
-          <h2 className="title-md text-center lg:text-left">
-            {sectionData.title}
+          <h2
+            id="accommodation-section-title"
+            className="title-md text-center lg:text-left"
+          >
+            {copyData.title}
           </h2>
-          <p className="body-md mt-6">{sectionData.description}</p>
+          <p className="body-md mt-6">{copyData.description}</p>
         </header>
 
         <AccommodationRequestForm />
