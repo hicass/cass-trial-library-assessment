f8a27e76c11ca1e7f7991691a0b65e1f517e4f7d
hicass <hicass.dev@gmail.com>
2025-07-28 17:43:44 -0700

    Update the padding on the ContentContainer and remove the padding from the HomePageContent to ensure consistent spacing across all sections and prevent double padding on the homepage layout

--- src/components/containers/ContentContainer.tsx
+++ src/components/containers/ContentContainer.tsx
@@ -6,5 +6,7 @@ interface ContentContainerProps {
 
 // A reusable responsive container with max-width and horizontal padding
 export const ContentContainer = ({ children }: ContentContainerProps) => {
-  return <div className="w-full px-4 sm:px-6 lg:max-w-6xl mx-auto">{children}</div>;
+  return (
+    <div className="w-full px-4 md:px-6 lg:max-w-6xl mx-auto">{children}</div>
+  );
 };
--- src/pages/home/HomePageContent.tsx
+++ src/pages/home/HomePageContent.tsx
@@ -5,7 +5,7 @@ import { Hero } from './components/Hero';
 
 export const HomePageContent = () => {
   return (
-    <main className="h-screen w-screen flex flex-col items-center overflow-y-visible gap-36 p-6 overflow-hidden relative">
+    <main className="h-screen w-screen flex flex-col items-center overflow-y-visible gap-36 overflow-hidden relative">
       <Hero />
       <CompanyIntro />
       <WeekOutlookSection />
